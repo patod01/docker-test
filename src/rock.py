d1ad606ffd9567375c29bb0a8246c241ecb8a6f9
patod01 <patocv2@hotmail.com>
2025-03-21 11:46:24 -0300

    other flush added

--- src/rock.py
+++ src/rock.py
@@ -9,8 +9,8 @@ def go_default(error):
 ### Real sh1t ###
 @route('/')
 def landing():
-     print("i'm world |m|,", flush=True)
-     print(f'addr: {request.remote_addr} and route: {request.remote_route} in bottle :D')
+     print("i'm world |m|," flush=True)
+     print(f'addr: {request.remote_addr} and route: {request.remote_route} in bottle :D', flush=True)
      return static_file('index.html', root='.')
 
 if __name__ == '__main__':
